c93c7e298c095b62c9dc767f1086a3dc5738d898
Cayt Schlichting <caytschlichting@gmail.com>
2022-03-31 15:00:24 -0700

    added comments to limitExercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,6 +3,16 @@ USE employees;
 SELECT DISTINCT last_name FROM employees 
 ORDER BY last_name DESC
 LIMIT 10;
+-- 'Zykh'
+-- 'Zyda'
+-- 'Zwicker'
+-- 'Zweizig'
+-- 'Zumaque'
+-- 'Zultner'
+-- 'Zucker'
+-- 'Zuberek'
+-- 'Zschoche'
+-- 'Zongker'
 
 #3
 SELECT first_name, last_name FROM employees
@@ -23,7 +33,7 @@ SELECT first_name, last_name FROM employees
 WHERE hire_date LIKE '199%'
 	AND birth_date LIKE '%12-25'
 ORDER BY hire_date #asc/oldest first
-LIMIT 5 OFFSET 45;
+LIMIT 5 OFFSET 45; #NOTE: This makes it start on result 46
 /*
 OFFSET = (#OF PAGES TO SKIP) * (LIMIT [AKA number of results per page])
 Since we are saying the limit is 5 per page, and we want to skip 9 pages,
